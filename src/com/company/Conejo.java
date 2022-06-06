18558e851029661ea24d3d42ee9f11839af12344
Daniel Chomon <a20180805@pucp.edu.pe>
2022-06-05 21:10:53 -0500

    clase conejo creada

---
+++ src/com/company/Conejo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Conejo extends Animal{
+
+    private String numOrejas;
+
+    public String getNumOrejas() {
+        return numOrejas;
+    }
+
+    public void setNumOrejas(String numOrejas) {
+        this.numOrejas = numOrejas;
+    }
+}
