33314bb89aa4a8b896e9bd0c1168f0a6430ac0b4
Daniel Chomon <a20180805@pucp.edu.pe>
2022-06-05 21:08:31 -0500

    clase animal modificada

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -3,6 +3,15 @@ package com.company;
 public class Animal {
     private String raza;
     private String nombre;
+    private int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
     public String getNombre() {
         return nombre;
